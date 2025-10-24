837d6b4f1ddb7905f64b06cd2f158836e6871daa
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-24 22:34:13 +0200

    fix ex02

--- 0 - Starting /ex02/find_ft_type.py
+++ 0 - Starting /ex02/find_ft_type.py
@@ -1,5 +1,12 @@
 def all_thing_is_obj(object: any) -> int:
 	"""Prints the object types and returns 42"""
 	obj_type = type(object)
-	print(f'{obj_type.__name__.capitalize()} : {obj_type}')
+
+	if isinstance(object, (list, dict, tuple, set)):
+		print(f'{obj_type.__name__.capitalize()} : {obj_type}')
+	elif isinstance(object, str):
+		print(f'{object} is in the kitchen : {obj_type}')
+	else:
+		print("Type not Found")
+
 	return 42
