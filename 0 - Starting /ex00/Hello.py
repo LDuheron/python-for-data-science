84c4ba1eccaf2c05a184fec2d5a5335cd01f1b97
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-24 21:58:27 +0200

    add gitignore + finish ex02

--- 0 - Starting /ex00/Hello.py
+++ 0 - Starting /ex00/Hello.py
@@ -3,14 +3,18 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
+# Modify ft_list
 ft_list.pop()
 ft_list.append("World!")
 
+# Overwrite ft_tuple
 ft_tuple = ("Hello", "France!")
 
-ft_set.add('Paris!')
+# Modify ft_set
 ft_set.discard("tutu!")
+ft_set.add('Paris!')
 
+# Modify ft_dict["Hello"]
 ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
