7180bcdd613aa8c6a842f7b66d8788ca5c22a655
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-25 17:21:25 +0200

    Ex05 finish

--- 0 - Starting /ex04/whatis.py
+++ 0 - Starting /ex04/whatis.py
@@ -1,5 +1,10 @@
 import sys
 
+argument_count = len(sys.argv)
+
+if argument_count == 1:
+	sys.exit(0)
+
 try:
 	if len(sys.argv) != 2:
 		raise AssertionError("more than one argument is provided")
@@ -13,6 +18,8 @@ try:
 
 except ValueError as error:
 	print(f"AssertionError: argument is not an integer")
+	sys.exit(1)
 
 except Exception as error:
 	print(f"AssertionError: {error}") 
+	sys.exit(1)
