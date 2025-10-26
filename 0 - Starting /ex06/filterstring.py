9219d7060865f4b5de8ca3d6314ddacc1907fecd
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-26 15:22:55 +0100

    in ex06: block strings contianing special char

--- 0 - Starting /ex06/filterstring.py
+++ 0 - Starting /ex06/filterstring.py
@@ -1,4 +1,5 @@
 import sys
+import string
 from ft_filter import ft_filter
 
 
@@ -11,10 +12,19 @@ def main():
 
         if input_str.isdigit():
             raise AssertionError("the arguments are bad")
+        
+        invisible_chars = {
+            "\t", "\n", "\r", "\v", "\f",
+            "\u00A0", "\u2000", "\u2001", "\u2002", "\u2003", "\u2004", "\u2005",
+            "\u2006", "\u2007", "\u2008", "\u2009", "\u200A", "\u200B",
+            "\u202F", "\u205F", "\u3000", "\uFEFF"
+        }
 
+        for char in input_str:
+            if char in string.punctuation or char in invisible_chars:
+                raise AssertionError("the arguments are bad")
 
         input_number = int(sys.argv[2])
-
 
         words = input_str.split()
 
