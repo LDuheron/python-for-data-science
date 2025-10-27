c0b86cc4a22cce3fea9f2ef36c4b2a692cfcb296
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-27 19:02:13 +0100

    Finish ex08

--- 0 - Starting /ex08/Loading.py
+++ 0 - Starting /ex08/Loading.py
@@ -1,26 +1,32 @@
 import time
 
-def get_percent(total: int):
-	for i in range(total):
-		percent = (i + 1) / total
-		yield percent
 
+def print_loading_bar(percent: float, total: int):
+    """Display a progress bar in the terminal with the percentage
+    completed and the count of elements processed.
 
-def	print_loading_bar(percent: int, total: int):
-	bar_len = 30
-	bar = int((percent * 30))
-	blank = bar_len - bar
-	bar_str = "=" * bar + " " * blank 
-	print(f"\r{int(percent * 100)}%| [{bar_str}] | {int(percent * total)}/{total}", end="", flush=True)
-	time.sleep(0.1)
-
-# if __name__ == "__main__":
-# 	total = 10
-# 	for n in get_percent(total):
-# 		print_loading_bar(n, total)
+    Args:
+        percent (float): Progress as a float between 0.0 and 1.0
+        total (int): Total number of elements to process
+    """
+    bar_len = 70
+    bar = int((percent * bar_len))
+    blank = bar_len - bar
+    bar_str = "=" * bar + ">" + " " * blank
+    text = (
+        f"\r{int(percent * 100):3d}%| "
+        f"[{bar_str}] | {int(percent * total)}/{total}"
+    )
+    print(text, end="", flush=True)
+    time.sleep(0.1)
 
 
 def ft_tqdm(lst: range) -> None:
-	total = len(lst)
-	for n in get_percent(total):
-		print_loading_bar(n, total)
+    """Decorate an iterable object, returning an iterator which acts exactly
+    like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested."""
+    total = len(lst)
+    for i, elem in enumerate(lst, start=1):
+        percent = i / total
+        print_loading_bar(percent, total)
+        yield i
