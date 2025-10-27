062bc78dcf291c8ddd763193ab88aa474c7bb541
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-27 17:28:55 +0100

    In ex01: fixed formats

--- 0 - Starting /ex01/format_ft_time.py
+++ 0 - Starting /ex01/format_ft_time.py
@@ -2,8 +2,7 @@
 from datetime import datetime
 
 now = datetime.now()
-jan_1970 = datetime(1970, 1, 1)
-delta = now - jan_1970
+jan_70 = now.timestamp()
 
-print(f'Seconds since January 1, 1970: {delta.total_seconds()} or {delta.total_seconds():e} in scientific notation')
+print(f'Seconds since January 1, 1970: {jan_70:,.3f} or {jan_70:2e} in scientific notation')
 print(f'{now.strftime("%b %d %Y")}')
