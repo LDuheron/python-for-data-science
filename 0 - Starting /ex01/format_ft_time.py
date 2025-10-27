5304decd2868def3840001501698c212b75d7401
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-27 17:07:39 +0100

    WIP

--- 0 - Starting /ex01/format_ft_time.py
+++ 0 - Starting /ex01/format_ft_time.py
@@ -2,6 +2,8 @@
 from datetime import datetime
 
 now = datetime.now()
+jan_1970 = datetime(1970, 1, 1)
+delta = now - jan_1970
 
-print(f'Seconds since January 1, 1970: {now.timestamp()} or {now.timestamp():e} in scientific notation')
+print(f'Seconds since January 1, 1970: {delta.total_seconds()} or {delta.total_seconds():e} in scientific notation')
 print(f'{now.strftime("%b %d %Y")}')
--- 0 - Starting /ex08/Loading.py
+++ 0 - Starting /ex08/Loading.py
@@ -9,18 +9,18 @@ def get_percent(total: int):
 def	print_loading_bar(percent: int, total: int):
 	bar_len = 30
 	bar = int((percent * 30))
-	blank = int((1 - percent) * bar_len)
-	bar_str = bar * "=" + blank * " "
-	print(f"{percent * 100}%| [ {bar}{blank}] | {percent * total}/{total}", end="\r", flush=True)
-	time.sleep(1)
+	blank = bar_len - bar
+	bar_str = "=" * bar + " " * blank 
+	print(f"\r{int(percent * 100)}%| [{bar_str}] | {int(percent * total)}/{total}", end="", flush=True)
+	time.sleep(0.1)
 
-if __name__ == "__main__":
-	total = 10
+# if __name__ == "__main__":
+# 	total = 10
+# 	for n in get_percent(total):
+# 		print_loading_bar(n, total)
+
+
+def ft_tqdm(lst: range) -> None:
+	total = len(lst)
 	for n in get_percent(total):
 		print_loading_bar(n, total)
-
-
-# def ft_tqdm(lst: range) -> None:
-# 	total = len(lst)
-# 	n = get_percent(total)
-# 	print_loading_bar(n, total)
