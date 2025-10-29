01b3773a9f8b355aec729fa092e63beebb91d76c
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-29 12:42:07 +0100

    Array/ex00: uncommented tester

--- 1 - Array/ex00/tester.py
+++ 1 - Array/ex00/tester.py
@@ -2,32 +2,33 @@ from give_bmi import give_bmi, apply_limit
 
 print(give_bmi.__doc__)
 print(apply_limit.__doc__)
-# # Valid values
-# height = [2.71, 1.15]
-# weight = [165.3, 38.4]
 
-# bmi = give_bmi(height, weight)
-# print("Valid values")
-# print(bmi, type(bmi))
-# print(apply_limit(bmi, 26))
+# Valid values
+height = [2.71, 1.15]
+weight = [165.3, 38.4]
 
-# # ZeroDivisionError
-# height = [2.71, 0]
-# weight = [165.3, 38.4]
+bmi = give_bmi(height, weight)
+print("Valid values")
+print(bmi, type(bmi))
+print(apply_limit(bmi, 26))
 
-# print("\nZeroDivisionError")
-# bmi = give_bmi(height, weight)
+# ZeroDivisionError
+height = [2.71, 0]
+weight = [165.3, 38.4]
 
-# # Different lenghts error
-# height = [2.71]
-# weight = [165.3, 38.4]
+print("\nZeroDivisionError")
+bmi = give_bmi(height, weight)
 
-# print("\nDifferent len lists")
-# bmi = give_bmi(height, weight)
+# Different lenghts error
+height = [2.71]
+weight = [165.3, 38.4]
 
-# # String in list
-# height = [2.71, "a"]
-# weight = [165.3, 38.4]
+print("\nDifferent len lists")
+bmi = give_bmi(height, weight)
 
-# print("\nString in list error")
-# bmi = give_bmi(height, weight)
+# String in list
+height = [2.71, "a"]
+weight = [165.3, 38.4]
+
+print("\nString in list error")
+bmi = give_bmi(height, weight)
