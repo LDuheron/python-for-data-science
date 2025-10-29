b7be5c23036b313fecb743983e454a99f6d66cbc
Lisa DUHERON <lisa.duheron@dauphine.eu>
2025-10-29 12:41:34 +0100

    Array/ex00: Finish exercice

--- 1 - Array/ex00/give_bmi.py
+++ 1 - Array/ex00/give_bmi.py
@@ -1,44 +1,67 @@
-import numpy as np
+def give_bmi(
+        height: list[int | float], weight: list[int | float]
+        ) -> list[int | float]:
+    """Calculate BMI values given lists of height and weight.
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+Args:
+    height (list[int | float]): A list of height values.
+    weight (list[int | float]): A list of weight values.
+
+Returns:
+    list[int | float] : A list of BMI values calculate from
+    the given height and weight pairs.
+"""
     try:
-        assert isinstance(height, list), "arguments must be lists of integers or floats"
+        assert isinstance(height, list), "arguments must be lists"
 
-        assert isinstance(weight, list), "arguments must be lists of integers or floats"
+        assert isinstance(weight, list), "arguments must be lists"
 
         assert len(height) == len(weight), "lists must be the same size"
 
-	    assert all(
-        isinstance(item, (int, float)) and item != 0 for item in height
-    ), "the arguments must be of type int or float and != 0"
+        assert all(
+            isinstance(item, (int, float)) for item in height
+            ), "elements of the list must be integers or floats"
 
- 	    assert all(
-        isinstance(item, (int, float)) for item in weight
-    ), "the arguments must be of type int or float"
+        assert all(
+            isinstance(item, (int, float)) for item in weight
+            ), "the arguments must be lists of elements type int or float"
 
-		height_array = np.array(height)
-		weight_array = np.array(weight)
+        bmi = []
+        for elem_w, elem_h in zip(weight, height):
+            bmi.append(elem_w/(elem_h**2))
 
-        bmi_list = []
-        for i in range(height):
-            bmi_list.append(weight[i]/(height[i]**2))
-        
-        return bmi_list
-
+        return bmi
 
     except AssertionError as error:
         print(f"AssertionError: {error}")
+
+    except ZeroDivisionError as error:
+        print(f"ZeroDivisionError: {error}")
 
     except Exception as error:
         print(f"Error: {error}")
 
 
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """Apply a limit on BMI values and return whether each value is below it.
 
+Args:
+    bmi (list[int | float]): A list of ints and floats representing BMI values
+    limit (int): The BMI threshold to compare against.
 
-height = [4, 4, 4]
-weight = [4, 4, 4]
+Returns:
+    A list of booleans where each element is True if the corresponding
+    value is below the limit, otherwise False.
+"""
+    assert isinstance(bmi, list), "first argument must be a list"
 
+    assert isinstance(limit, int), "second argument must be an integer"
 
-give_bmi(height, weight)
-# def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-#your code here
+    assert all(
+        isinstance(item, (int, float)) for item in bmi
+    ), "elements of the list must be integers or floats"
+
+    is_above_limit = []
+    for elem in bmi:
+        is_above_limit.append(elem > limit)
+    return is_above_limit
--- 1 - Array/ex00/tester.py
+++ 1 - Array/ex00/tester.py
@@ -1,8 +1,33 @@
-rom give_bmi import give_bmi, apply_limit
+from give_bmi import give_bmi, apply_limit
 
-height = [2.71, 1.15]
-weight = [165.3, 38.4]
+print(give_bmi.__doc__)
+print(apply_limit.__doc__)
+# # Valid values
+# height = [2.71, 1.15]
+# weight = [165.3, 38.4]
 
-bmi = give_bmi(height, weight)
-print(bmi, type(bmi))
-print(apply_limit(bmi, 26))
+# bmi = give_bmi(height, weight)
+# print("Valid values")
+# print(bmi, type(bmi))
+# print(apply_limit(bmi, 26))
+
+# # ZeroDivisionError
+# height = [2.71, 0]
+# weight = [165.3, 38.4]
+
+# print("\nZeroDivisionError")
+# bmi = give_bmi(height, weight)
+
+# # Different lenghts error
+# height = [2.71]
+# weight = [165.3, 38.4]
+
+# print("\nDifferent len lists")
+# bmi = give_bmi(height, weight)
+
+# # String in list
+# height = [2.71, "a"]
+# weight = [165.3, 38.4]
+
+# print("\nString in list error")
+# bmi = give_bmi(height, weight)
